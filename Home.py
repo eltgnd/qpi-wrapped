b47835752fb4c4204e16c33a046a96f3bb3dff3b
Val Eltagonde <valeltagonde@gmail.com>
2023-12-28 19:30:11 +0800

    fix QPI computation

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ import pandas as pd
 letters = {'A':4.00, 'B+':3.50, 'B':3.00, 'C+':2.50, 'C':2.00, 'D':1.00, 'F':0.00, 'W':0.00}
 
 
-st.set_page_config(layout='wide')
+# st.set_page_config(layout='wide')
 
 st.title('🤖 QPI Bot')
 st.text('description')
@@ -19,18 +19,35 @@ def error(message):
 def correct_format(grade):
     return True
 def part_of_qpi(grade):
+    d = grade.split('	')
+
+    subj_conditions = ['PHYED', 'NSTP']
+    for s in subj_conditions:
+        l = len(s)
+        if d[3][:l] == s:
+            return False
+
+    grade_conditions = ['WP', 'S']
+    for c in grade_conditions:
+        if c in d:
+            return False        
     return True
 
 def compute_qpi(df):
-    # error
-    df['Weight'] = df['Units'] * df['Final Grade']
-    return round(df['Weight'].sum() / df['Units'].sum(),2)
+    df['Units'] = pd.to_numeric(df['Units'], errors='coerce')
+    df['Numerical Grade'] = df['Final Grade'].map(letters)
+    total_units = df['Units'].sum()
+
+    df['Weighted Grade'] = df['Units'] * df['Numerical Grade']
+    weighted_grade = round(df['Weighted Grade'].sum() / total_units, 2)
+    st.write("Weighted Average:", weighted_grade)
 
 # Text processing
 if s and button:
     columns = 'School Year	Sem	Course	Subject Code	Course Title	Units	Final Grade'.split('	')
     grades = s.split('\n')
     data = [grade.split('	') for grade in grades if correct_format(grade) and part_of_qpi(grade)]
+    print(data[0])
     full_df = pd.DataFrame(data, columns=columns)
     df = full_df[['Subject Code', 'Units', 'Final Grade']]
 
