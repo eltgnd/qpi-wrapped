2823c3515c22c6ea05dffdfa6f21e61f4c406426
Val Eltagonde <valeltagonde@gmail.com>
2024-01-05 18:39:12 +0800

    add placeholder

--- Home.py
+++ Home.py
@@ -77,8 +77,8 @@ if 'submitted' not in st.session_state:
 
 # Form
 with st.container(border=True):
-    user_input = st.text_area('Input your grades from AISIS', 
-        # placeholder=sample_data,
+    user_input = st.text_area('Input your grades from AISIS.', 
+        placeholder='Refresh this page if you cannot input your text!',
         disabled=st.session_state.submitted,
         key='str'
     )
