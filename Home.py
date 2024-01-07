20f8d79313e08fffc14916f541a9d1a1c057b623
Val Eltagonde <valeltagonde@gmail.com>
2024-01-07 21:52:25 +0800

    fix description

--- Home.py
+++ Home.py
@@ -477,9 +477,9 @@ try:
             st.toast('Feedback recorded!', icon='📝')
 
     # Announcement
-    add_vertical_space(2)
+    add_vertical_space(1)
     with st.expander('What\'s next? 👀', expanded=False):
-        st.write('Support for other universities\' is coming soon!')
+        st.write('Support for other universities\' grades is coming soon!')
 
 except Exception as e:
     add_vertical_space(2)
